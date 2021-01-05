53d5e533ce06ddc86737dad9d0d82c43d7e18b78
madhurimhaske7 <madhurimhaske7@gmail.com>
2021-01-05 11:38:01 +0530

    modified

--- src/main/java/com/mycompany/gitjenkin/MainClass.java
+++ src/main/java/com/mycompany/gitjenkin/MainClass.java
@@ -14,6 +14,7 @@ public class MainClass {
     public static void main(String[] args) {
         
         System.out.println("Hello Jenkins!");
+        System.out.println("Hello Modified!");
     }
     
 }
